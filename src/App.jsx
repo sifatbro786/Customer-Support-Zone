10a404c7ea91dd19f6dad74a0c9200daa04b252f
sfatbro786 <solaimanislamsifat@gmail.com>
2026-03-04 15:09:07 +0600

    make 10 dummy ticket data into json format

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold">Customer Support Zone</h1>
-            <button className="btn btn-primary">Primary</button>
+            
         </>
     );
 }
